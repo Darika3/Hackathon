0c24db6206dc35a0d7fbaf21971ea81a4bd4bf9c
Darika3 <darika.zholborsova@gmail.com>
2023-04-13 21:10:19 +0600

    .

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-let form = document.querySelector(".inputs")
+let form = document.querySelector("#inputs")
 let inpImg = document.querySelector("#inpImage");
 let inpTrailer = document.querySelector("#inpTrailer")
 let inpName = document.querySelector("#inpName");
 let inpDesc = document.querySelector("#inpDesc");
 let btnAdd = document.querySelector("#btnAdd");
+let inpCategory= document.querySelector("#inpCategory");
 let API = "http://localhost:8000/movies";
 let cardsContainer = document.querySelector("#cards");
-let currentPage = 1;
-let pageLength = 1;
+// let currentPage = 1;
+// let pageLength = 1;
 // let filterValue="Все";
 
 
@@ -20,17 +21,20 @@ form.addEventListener("submit", (e) => {
       !inpName.value.trim() ||
       !inpImg.value.trim() ||
       !inpDesc.value.trim() ||
-      !inpTrailer.value.trim()
+      !inpTrailer.value.trim()||
+      !inpCategory.value.trim()
     ) {
       alert("Заполните все поля!");
-      return;
     }
     //   Создаём новый объект и туда добавляем значения наших инпутов
   let newCard = {
     title: inpName.value,
     image: inpImg.value,
-    description: inpDesc.value
+    description: inpDesc.value,
+    trailer: inpTrailer.value,
+    category: inpCategory.value,
   };
+//   console.log(inpCategory.value);
   createCard(newCard);
 });
 
@@ -44,8 +48,8 @@ async function createCard(objProf) {
       body: JSON.stringify(objProf),
     });
     readCard();
-  
-    let inputs = document.querySelectorAll("form input");
+  inpCategory.value="Возраст";
+    let inputs = document.querySelectorAll(".inputs input");
     inputs.forEach((elem) => {
       elem.value = "";
     });
@@ -62,7 +66,7 @@ async function readCard(search = "") {
       <div class="grad">
             <div class="card" style="width: 20rem; height: 43rem">
               <img
-                style="height: 31em; width: 100%"
+                style="height: 450px; width: 100%"
                 src="${elem.image}"
                 class="card-img-top"
                 alt="..."
@@ -70,19 +74,20 @@ async function readCard(search = "") {
               />
               <div class="card-body">
                 <h5 class="card-title">${elem.title}</h5>
+                <p>${elem.category}</p>
                 <p class="card-text">
                 ${elem.description}
                 </p>
-                <button style="color: white;
+                <button  style="color: white;
                 background-color: rgb(141, 36, 36);
                 border: none;
                 border-radius: 5px;
-                width: 40%;" onclick="showModalEdit(${elem.id}) class="btn">Edit</button>
-                <button style="color: white;
+                width: 40%;" onclick="showModalEdit(${elem.id})" class="btn">Edit</button>
+                <button onclick="deleteCard(${elem.id})" style="color: white;
                 background-color: rgb(141, 36, 36);
                 border: none;
                 border-radius: 5px;
-                width: 40%;" onclick="deleteCard(${elem.id}) class="btn">Delete</button>
+                width: 40%;"  class="btn"  >Delete</button>
               </div>
             </div>      
           </div>
@@ -98,8 +103,68 @@ async function readCard(search = "") {
 // Delete - удаление одного элемента по id
 
 async function deleteCard(id) {
+    console.log('fdsa');
     await fetch(`${API}/${id}`, {
       method: "DELETE",
     });
     readCard();
   }
+
+  
+// ! Edit
+// let editBtns = document.querySelector(".btnEdit");
+let modal = document.querySelector("#modalEdit");
+let closeBtn = document.querySelector("#closeEditModal");
+let editInpName = document.querySelector("#editInpTitle");
+let editInpImage = document.querySelector("#editInpImage");
+let editInpTrailer = document.querySelector("#editInpTrailer");
+let editInpDesc = document.querySelector("#editInpDesc");
+let editInpCategory = document.querySelector("#editInpCategory");
+let editForm = document.querySelector("#editForm");
+let btnSave = document.querySelector("#saveEditModal");
+
+async function showModalEdit(id) {
+  modal.style.display = "flex";
+  let res = await fetch(`${API}/${id}`);
+  let data = await res.json();
+  console.log(data);
+  editInpName.value = data.title;
+  editInpImage.value = data.image;
+  editInpTrailer.value = data.trailer;
+  editInpDesc.value = data.description;
+  editInpCategory.value = data.category;
+  btnSave.setAttribute("id", data.id);
+}
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let editedCard = {
+    title: editInpName.value,
+    image: editInpImage.value,
+    trailer: editInpTrailer.value,
+    description: editInpDesc.value,
+    category: editInpCategory.value,
+  };
+  console.log(btnSave.id);
+  editCardFunc(editedCard, btnSave.id);
+});
+
+async function editCardFunc(editedCard, id) {
+  try {
+    await fetch(`${API}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(editedCard),
+    });
+    modal.style.display = "none";
+    readCard();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
