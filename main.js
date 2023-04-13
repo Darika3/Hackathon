b7fd15f150e3a62e5522c24aaec74be03915de29
Aidai Nadyrbekova <aidai.nadyr@gmail.com>
2023-04-13 17:03:04 +0600

    .

---
+++ main.js
@@ -0,0 +1,6 @@
+let inputAdd = document.querySelector(".inputs")
+let inpImage = document.querySelector("#inpImage");
+let inpName = document.querySelector("#inpName");
+let inpDesc = document.querySelector("#inpDesc");
+let btnAdd = document.querySelector("#btnAdd");
+
